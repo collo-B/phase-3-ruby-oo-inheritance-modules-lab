2ddd60fa73a3cc6a27b89ae866a11b4ad986bccd
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-08-29 05:24:09 +0300

    commit tree 9a081ea6e9a2166917d429fec04b8316fdde4932

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
-
   def initialize
     @@artists << self
     @songs = []
   end
-
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
-
   def self.all
     @@artists
   end
@@ -22,16 +24,21 @@ class Artist
   def self.reset_all
     self.all.clear
   end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
   def self.count
     self.all.count
   end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
     song.artist = self
   end
-
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
@@ -39,4 +46,7 @@ class Artist
   def to_param
     name.downcase.gsub(' ', '-')
   end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
